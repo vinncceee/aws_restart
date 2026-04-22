d0ec06bdb6fea7bdcf8b518495ab8a86ca5a961e
EC2 Default User <ec2-user@ip-10-16-10-109.us-west-2.compute.internal>
2026-04-22 16:04:25 +0000

    finished Introducing System Administration with Python

---
+++ Introducing System Administration with Python.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+subprocess.run(["ls","-l"])
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
